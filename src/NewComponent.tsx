50273fad105f1848685e4b75e0ccbb965750effc
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 19:14:19 +0300

    first commit

---
+++ src/NewComponent.tsx
@@ -0,0 +1,18 @@
+type NewComponentPropsType = {
+    students: Array<StudentType>
+}
+
+
+}
+
+
+export const NewComponent = (props: NewComponentPropsType) => {
+    return (
+        <ul>
+
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
